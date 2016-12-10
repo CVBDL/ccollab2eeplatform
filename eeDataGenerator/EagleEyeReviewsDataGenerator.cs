488f4659a640f5f7d312f06acad02d734e78611c
zhongsp <patrick.zhongsp@gmail.com>
2016-12-10 08:02:07 +0800

    refactor

--- eeDataGenerator/EagleEyeReviewsDataGenerator.cs
+++ eeDataGenerator/EagleEyeReviewsDataGenerator.cs
@@ -53,6 +53,30 @@ namespace eeDataGenerator
             return reviewsQuery.ToList<string[]>();
         }
 
+        private void PutDataTableToEagleEye(string chartId, string json)
+        {
+            // API: <https://github.com/CVBDL/EagleEye-Docs/blob/master/rest-api/rest-api.md#edit-data-table>
+
+            log.Info("Sending Review Count By Month Data to Server ...");
+
+            try
+            {
+                StringContent payload = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = httpClient.PutAsync(settings.EagleEyeApiRootEndpoint + "charts/" + chartId + "/datatable", payload).Result;
+                response.EnsureSuccessStatusCode();
+
+                // use for debugging
+                var responseContent = response.Content;
+                string responseBody = responseContent.ReadAsStringAsync().Result;
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                log.Info("Error: Put data table to chart with id '" + chartId + "'");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+
         private void GenerateReviewCountByMonth()
         {
             log.Info("Generating: Review Count By Month ...");
@@ -71,21 +95,7 @@ namespace eeDataGenerator
                 orderby month.Key ascending
                 select new { Month = month.Key, Count = month.Count() };
 
-            List<object[]> datatable = new List<object[]>();
-
-            datatable.Add(new object[] { "Month", "Count" });
-
-            foreach (var date in query)
-            {
-                datatable.Add(new object[] { date.Month, date.Count });
-                log.Info("Month: " + date.Month + ", Count: " + date.Count);
-            }
-            
-            string json = JsonConvert.SerializeObject(new Chart(datatable.ToArray()));
-
-            log.Info(json); // {"datatable":[["Month","Count"],["2016-07",1],["2016-08",2],["2016-09",1]]}
-
-            // Expected format:
+            // Expected data table format:
             // https://github.com/CVBDL/EagleEye-Docs/blob/master/rest-api/rest-api.md#edit-data-table
             // {
             //   "datatable": [
@@ -96,26 +106,19 @@ namespace eeDataGenerator
             //   ]
             // }
 
-            log.Info("Sending Review Count By Month Data to Server ...");
+            List<object[]> datatable = new List<object[]>();
 
-            // calling EagleEye API to update chart
+            datatable.Add(new object[] { "Month", "Count" });
 
-            //try
-            //{
-            //    var chartId = chartSettings.ChartId;
-            //    StringContent payload = new StringContent(json, Encoding.UTF8, "application/json");
-            //    HttpResponseMessage response = httpClient.PutAsync(settings.EagleEyeApiRootEndpoint + "charts/" + chartId + "/datatable", payload).Result;
-            //    response.EnsureSuccessStatusCode();
-            //    var responseContent = response.Content;
-            //    string responseBody = responseContent.ReadAsStringAsync().Result;
+            foreach (var date in query)
+            {
+                datatable.Add(new object[] { date.Month, date.Count });
+                Console.WriteLine("Month: " + date.Month + ", Count: " + date.Count);
+            }
+            
+            string json = JsonConvert.SerializeObject(new Chart(datatable.ToArray()));
 
-            //    Console.WriteLine(responseBody);
-            //}
-            //catch (HttpRequestException e)
-            //{
-            //    Console.WriteLine("\nUpdate task state error!");
-            //    Console.WriteLine("Message :{0} ", e.Message);
-            //}
+            Console.WriteLine(json); // {"datatable":[["Month","Count"],["2016-07",1],["2016-08",2],["2016-09",1]]}
 
             log.Info("Generating: Review Count By Month ... Done.");
         }
